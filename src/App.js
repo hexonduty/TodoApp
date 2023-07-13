25f8d32506da4976cdf687c47d36c53a21e65bf7
hexonduty <masterhexonduty@gmail.com>
2023-07-13 03:40:02 +0300

    removed zunkfolders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+let id = 0;
+
 function App() {
+  const styleFont = {color: "#fff", fontSize: "23px"}
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <label htmlFor="todo" style={styleFont}>TODO APP</label>
+      <input type='t//ext' id='todo' placeholder='Please Write Some Notes' value={todo} onChange={(e) => {
+        setTodo(e.target.value);
+      }} />
+      <button className='todoButton' onClick={() => {
+        setTodos([...todos, {
+          id: id++,
+          name: todo,
+          isComplated: false
+        }]);
+      }}>Save</button>
+      <ul>
+        {todos.map(todo => {
+          return (
+            <>
+              <div className='todoside'>
+                <li className={todo.isComplated ? 'completed':'not'} key={todo.id}>{todo.name}</li>
+                <div className='btnside'>
+                  <button className='todoDelete' id={todo.id} onClick={
+                    () => {
+                      setTodos(todos.filter(a =>
+                        a.id !== todo.id
+                      ))
+                      console.log(todos);
+                    }
+                  }>Delete</button>
+                  <button className='todoComplete' id={todo.id} onClick={() =>
+                    setTodos(todos.map(todolar => {
+                      if(todolar.id == todo.id ){
+                        return {...todolar, isComplated: !todolar.isComplated }
+                      } else{
+                        return todolar
+                      }
+                    }))
+                  }>Complete</button>
+                </div>
+              </div>
+            </>
+          )
+        })}
+      </ul>
+      <div className='footer'>
+        <h2>This project was made by <a href='https://www.instagram.com/efecanacikgozz.js/'>MasterHexOnDuty</a> </h2>
+      </div>
     </div>
   );
 }
